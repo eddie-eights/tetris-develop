2f41469270a50b47c29906ed914726fdccb94b6d
eddie-eights <fuk8eightuda@gmail.com>
2023-02-16 17:13:49 +0900

    fix

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ let deleteMissions;
 let displayDeleteMissionsEle;
 
 function gameStart() {
+	config.clearPage.style.zIndex = -1;
+	config.clearPage.style.visibility = "hidden";
 	gameOver = false;
 	gameClear = false;
 	initializeField();
@@ -239,7 +241,7 @@ function setTetro() {
 }
 
 const timer = document.getElementById("timer");
-const TIME = 10;
+const TIME = 60;
 let startTime;
 let countDown;
 function startTimer() {
@@ -513,8 +515,8 @@ function isGameOver() {
 }
 
 function notifyUsersGameOver() {
-	message["title"] = "ゲームオーバー";
-	message["msg"] = "時間切れです！再度挑戦してみましょう";
+	message["title"] = "Game Over";
+	message["msg"] = "時間切れです...  再度挑戦してみましょう！";
 	document.getElementById("modal-btn").dispatchEvent(new Event("click"));
 }
 
@@ -528,9 +530,11 @@ function isGameClear() {
 }
 
 function notifyUsersGameClear() {
-	message["title"] = "ゲームクリア！";
-	message["msg"] = "おめでとうございます！あなたは価値ある人間です！";
+	message["title"] = "Game Clear !!";
+	message["msg"] = "おめでとうございます！あなたは価値のある人間です！";
 	document.getElementById("modal-btn").dispatchEvent(new Event("click"));
+	config.clearPage.style.visibility = "visible";
+	config.clearPage.style.zIndex = 100;
 }
 
 let exampleModal = document.getElementById("exampleModal");
@@ -586,76 +590,85 @@ document.onkeydown = function (e) {
 	drawAll();
 };
 
-particlesJS("particles-js",{
-	"particles":{
-		"number":{
-			"value":125,//この数値を変更すると紙吹雪の数が増減できる
-			"density":{
-				"enable":false,
-				"value_area":400
-			}
-		},
-		"color": {
-        "value": ["#EA5532", "#F6AD3C", "#FFF33F", "#00A95F", "#00ADA9", "#00AFEC","#4D4398", "#E85298"]//紙吹雪の色の数を増やすことが出来る
-		},
-		"shape":{
-			"type":"polygon",//形状はpolygonを指定
-			"stroke":{
-				"width":0,
-			},
-			"polygon":{
-				"nb_sides":5//多角形の角の数
-			}
-			},
-			"opacity":{
-				"value":1,
-				"random":false,
-				"anim":{
-					"enable":true,
-					"speed":20,
-					"opacity_min":0,
-					"sync":false
-				}
-			},
-			"size":{
-				"value":5.305992965476349,
-				"random":true,//サイズをランダムに
-				"anim":{
-					"enable":true,
-					"speed":1.345709068776642,
-					"size_min":0.8,
-					"sync":false
-				}
-			},
-			"line_linked":{
-				"enable":false,
-			},
-			"move":{
-				"enable":true,
-			"speed":10,//この数値を小さくするとゆっくりな動きになる
-			"direction":"bottom",//下に向かって落ちる
-			"random":false,//動きはランダムにならないように
-			"straight":false,//動きをとどめない
-			"out_mode":"out",//画面の外に出るように描写
-			"bounce":false,//跳ね返りなし
-				"attract":{
-					"enable":false,
-					"rotateX":600,
-					"rotateY":1200
-				}
-			}
-		},
-		"interactivity":{
-			"detect_on":"canvas",
-			"events":{
-				"onhover":{
-					"enable":false,
-				},
-				"onclick":{
-					"enable":false,
-				},
-				"resize":true
+particlesJS("clearPage", {
+	particles: {
+		number: {
+			value: 125, //この数値を変更すると紙吹雪の数が増減できる
+			density: {
+				enable: false,
+				value_area: 400,
 			},
 		},
-		"retina_detect":true
-	});
+		color: {
+			value: [
+				"#EA5532",
+				"#F6AD3C",
+				"#FFF33F",
+				"#00A95F",
+				"#00ADA9",
+				"#00AFEC",
+				"#4D4398",
+				"#E85298",
+			], //紙吹雪の色の数を増やすことが出来る
+		},
+		shape: {
+			type: "polygon", //形状はpolygonを指定
+			stroke: {
+				width: 0,
+			},
+			polygon: {
+				nb_sides: 5, //多角形の角の数
+			},
+		},
+		opacity: {
+			value: 1,
+			random: false,
+			anim: {
+				enable: true,
+				speed: 20,
+				opacity_min: 0,
+				sync: false,
+			},
+		},
+		size: {
+			value: 5.305992965476349,
+			random: true, //サイズをランダムに
+			anim: {
+				enable: true,
+				speed: 1.345709068776642,
+				size_min: 0.8,
+				sync: false,
+			},
+		},
+		line_linked: {
+			enable: false,
+		},
+		move: {
+			enable: true,
+			speed: 10, //この数値を小さくするとゆっくりな動きになる
+			direction: "bottom", //下に向かって落ちる
+			random: false, //動きはランダムにならないように
+			straight: false, //動きをとどめない
+			out_mode: "out", //画面の外に出るように描写
+			bounce: false, //跳ね返りなし
+			attract: {
+				enable: false,
+				rotateX: 600,
+				rotateY: 1200,
+			},
+		},
+	},
+	interactivity: {
+		detect_on: "canvas",
+		events: {
+			onhover: {
+				enable: false,
+			},
+			onclick: {
+				enable: false,
+			},
+			resize: true,
+		},
+	},
+	retina_detect: true,
+});
